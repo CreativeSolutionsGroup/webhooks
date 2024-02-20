4a7e10d187e94ab131fbb75401c9ba96021a1ccf
Sebastian Neiswanger <31674857+SebastianNeiswanger@users.noreply.github.com>
2024-02-20 13:18:19 -0500

    Update route.ts

--- app/route.ts
+++ app/route.ts
@@ -6,7 +6,7 @@ declare global {
       BASECAMP_REDIRECT_URI: string;
       BASECAMP_REFRESH_TOKEN: string;
       DATABASE_URL: string;
-      SMARTSHtEET_API: string;
+      SMARTSHEET_API: string;
     }
   }
 }
